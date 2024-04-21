dc5cb3f30bac540b38e6fc3cee7c618cf0e02d63
lsqtal <129973731+lsqtal@users.noreply.github.com>
2024-04-21 03:21:30 +0000

    modified:   exercises/primitive_types/primitive_types1.rs 	modified:   exercises/primitive_types/primitive_types2.rs 	modified:   exercises/primitive_types/primitive_types3.rs 	modified:   exercises/primitive_types/primitive_types4.rs 	modified:   exercises/primitive_types/primitive_types5.rs 	modified:   exercises/primitive_types/primitive_types6.rs 	modified:   exercises/quiz1.rs 	modified:   exercises/vecs/vecs1.rs 	modified:   exercises/vecs/vecs2.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,156,16,11,11,11,11,11,5,2,5,56,1,6,19,1];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
