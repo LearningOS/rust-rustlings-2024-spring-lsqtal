bd2f1dd5f118db43f9a623e0627710864d67562e
lsqtal <129973731+lsqtal@users.noreply.github.com>
2024-04-26 14:41:15 +0000

    modified:   exercises/conversions/as_ref_mut.rs 	modified:   exercises/conversions/from_into.rs 	modified:   exercises/conversions/from_str.rs 	modified:   exercises/conversions/try_from_into.rs 	modified:   exercises/conversions/using_as.rs 	modified:   exercises/tests/build.rs 	modified:   exercises/tests/tests5.rs 	modified:   exercises/tests/tests6.rs 	modified:   exercises/tests/tests7.rs 	modified:   exercises/tests/tests8.rs 	modified:   exercises/tests/tests9.rs

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -51,7 +51,31 @@ enum ParsePersonError {
 
 impl FromStr for Person {
     type Err = ParsePersonError;
-    fn from_str(s: &str) -> Result<Person, Self::Err> {
+    fn from_str(s: &str) -> Result<Person, Self::Err> 
+    {
+        if s.len() ==0  {
+            return Err(ParsePersonError::Empty)
+        }
+        let vecData: Vec<&str> = s.split(',').collect();
+        if vecData.len() !=2 {
+            return Err(ParsePersonError::BadLen)
+        }
+        let name=String::from(vecData[0]);
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName)
+        }
+        let age = match vecData[1].parse::<usize>() 
+        {
+            Ok(age)=>age,
+            Err(e) => return Err(ParsePersonError::ParseInt(e))
+        };
+        Ok(Person 
+            {
+            name,
+            age
+        })
+ 
+    
     }
 }
 
