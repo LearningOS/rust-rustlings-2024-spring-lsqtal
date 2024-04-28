2ece02222bfff48210a3d809db159a1840c7233e
lsqtal <129973731+lsqtal@users.noreply.github.com>
2024-04-28 08:44:39 +0000

    modified:   exercises/algorithm/algorithm10.rs 	modified:   exercises/tests/build.rs 	modified:   exercises/tests/tests7.rs 	modified:   exercises/tests/tests8.rs

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,6 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -30,6 +29,19 @@ impl Graph for UndirectedGraph {
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (from_node, to_node, weight) = edge;
+        self.add_node(from_node);
+        self.add_node(to_node);
+        
+        self.adjacency_table_mutable()
+            .entry(String::from(from_node))
+            .or_insert(Vec::new())
+            .push((String::from(to_node), weight));
+    
+        self.adjacency_table_mutable()
+            .entry(String::from(to_node))
+            .or_insert(Vec::new())
+            .push((String::from(from_node), weight));
     }
 }
 pub trait Graph {
@@ -38,10 +50,29 @@ pub trait Graph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+		//true
+        if self.adjacency_table().contains_key(node) {
+            false // Node already exists
+        } else {
+            self.adjacency_table_mutable().insert(String::from(node), Vec::new());
+            true // Node added successfully
+        }
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (from_node, to_node, weight) = edge;
+        self.add_node(from_node);
+        self.add_node(to_node);
+        
+        self.adjacency_table_mutable()
+            .entry(String::from(from_node))
+            .or_insert(Vec::new())
+            .push((String::from(to_node), weight));
+    
+        self.adjacency_table_mutable()
+            .entry(String::from(to_node))
+            .or_insert(Vec::new())
+            .push((String::from(from_node), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
